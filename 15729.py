a2551046bc7accc398ec02f8681a10393a3c7f7b
솜놜 <90139018+somnwal@users.noreply.github.com>
2023-01-09 22:45:18 +0900

    20230109
    20230109

--- 15729.py
+++ 15729.py
@@ -3,5 +3,22 @@
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-arr = list(map(int, sys.stdin.readline().rstrip().split()))
+target = list(map(str, sys.stdin.readline().rstrip().split()))
+text = ['0' for _ in range(n)]
 
+count = 0
+
+for i in range(n):
+    if text[i] != target[i]:
+        for j in range(i, i+3):
+            if j >= len(text):
+                break
+
+            if text[j] == '1':
+                text[j] = '0'
+            else:
+                text[j] = '1'
+
+        count += 1
+
+print(count)
