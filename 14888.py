73a72199dfdf38fbcfa25a4175cd4c99480d1a20
솜놜 <90139018+somnwal@users.noreply.github.com>
2023-01-09 22:13:10 +0900

    20230109

--- 14888.py
+++ 14888.py
@@ -2,3 +2,45 @@
 
 import sys
 
+n = int(sys.stdin.readline().rstrip())
+numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+init_op = list(map(int, sys.stdin.readline().rstrip().split()))
+
+arr = []
+
+
+def dfs_max(op, cur, i):
+    if i == len(numbers):
+        arr.append(cur)
+        return
+
+    for j in range(4):
+        if op[j] == 0:
+            continue
+
+        new_op = op[:]
+        new_op[j] -= 1
+
+        if j == 0:
+            dfs_max(new_op, cur + numbers[i], i + 1)
+        elif j == 1:
+            dfs_max(new_op, cur - numbers[i], i + 1)
+        elif j == 2:
+            dfs_max(new_op, cur * numbers[i], i + 1)
+        elif j == 3:
+            new_cur = cur
+
+            if cur < 0:
+                new_cur = cur * -1
+                new_cur = new_cur // numbers[i]
+                new_cur = new_cur * -1
+            else:
+                new_cur = new_cur // numbers[i]
+
+            dfs_max(new_op, new_cur, i + 1)
+
+
+dfs_max(init_op, numbers[0], 1)
+
+print(max(arr))
+print(min(arr))
---
+++ 15729.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/15729
+
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+
