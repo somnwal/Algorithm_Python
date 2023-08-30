f536f1ba234c0ed0b09e3eb7b6cb66f23f90012b
솜놜 <somnwal.kor@gmail.com>
2023-08-30 21:23:59 +0900

    20230830

--- _Example/배율.py
+++ _Example/배율.py
@@ -1,6 +1,6 @@
-n = [1.2, 1.3, 1.4, 1.5, 1.6, 1.02]
+n = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.02]
 
 for nn in n:
-    for i in range(2, 16):
+    for i in range(2, 31):
         print(nn, '^', i, '=', round(nn ** i, 2), '배')
     print()
