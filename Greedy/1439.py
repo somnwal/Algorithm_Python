afc891e51900fa31ca4444790e310838a3a4385c
Hi, I'm Somnwal <90139018+somnwal@users.noreply.github.com>
2024-03-24 18:35:00 +0900

    Update 1439.py

--- Greedy/1439.py
+++ Greedy/1439.py
@@ -2,16 +2,11 @@
 
 import sys
 
-text = sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
-count = 0
+s = input().rstrip()
 
-zc = sum(len(i) != 0 for i in text.split("1"))
-oc = sum(len(i) != 0 for i in text.split("0"))
+zc = sum(len(i) != 0 for i in s.split("1"))
+oc = sum(len(i) != 0 for i in s.split("0"))
 
-if zc >= oc:
-    count = oc
-else:
-    count = zc
-
-print(count)
+print(min(zc, oc))
